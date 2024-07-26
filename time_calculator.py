3bd2ba008f7cb1e57acef97a23be5d50ff75ea65
irrit-im <meyerovitch.idit@gmail.com>
2024-07-26 21:44:12 +0300

    clarified docstring

--- time_calculator.py
+++ time_calculator.py
@@ -2,7 +2,11 @@ from datetime import datetime, timedelta
 
 
 def add_seconds(secs: float) -> datetime:
-    """Calculates the exact time after a specified number of seconds have passed
-    secs: number of seconds to add"""
+    """Calculates the exact time after a specified number of seconds have passed.
+    Args:
+        secs: number of seconds to add.
+    Returns:
+        The updated datetime object."""
+
     current_time = datetime.now()
     return current_time + timedelta(seconds=secs)
