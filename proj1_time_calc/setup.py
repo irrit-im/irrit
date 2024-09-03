8140d246888b4a45aacf01ac8b0f056d4c2b7d8a
irrit-im <meyerovitch.idit@gmail.com>
2024-09-03 16:51:03 +0300

    deleted irrelevant setup args

--- proj1_time_calc/setup.py
+++ proj1_time_calc/setup.py
@@ -11,18 +11,8 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 setup(
     name="time_calcfreename",
     version="0.0.4",
-    author="Someone",
-    author_email="<mail@neuralnine.com>",
     description="Hi Mai",
     long_description_content_type="text/markdown",
     long_description="This python package contains a single function that calculates the exact time after a specified number of seconds have passed",
     packages=find_packages(),
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ],
 )
