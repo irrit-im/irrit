a7219f2f04c1f37c061f151999a5830b847e96c9
irrit-im <meyerovitch.idit@gmail.com>
2024-09-06 18:23:30 +0300

    Changed example to an I/O bound multi-threaded program

--- proj2_GIL/no_impact.py
+++ proj2_GIL/no_impact.py
@@ -1,1 +1,27 @@
-print("This programe is single-threaded and therefore is not affected by the GIL")
+from threading import Thread
+from time import time, sleep
+
+NUMBER = 100
+
+
+def wait(millis: int):
+    for i in range(millis):
+        sleep(0.01)
+
+
+# Multi-threaded
+t1 = Thread(target=wait, args=(NUMBER,))
+t2 = Thread(target=wait, args=(NUMBER,))
+start = time()
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+end = time()
+
+# Single-threaded
+start = time()
+wait(NUMBER)
+end = time()
+
+print("Run time: ", end - start)
