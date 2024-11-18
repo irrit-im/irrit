db247a2b289bb2c29a9984958eec1867ba374411
irrit-im <meyerovitch.idit@gmail.com>
2024-11-18 20:30:59 +0200

    created random board function

--- proj3_chess/main.py
+++ proj3_chess/main.py
@@ -1,29 +1,33 @@
 from chess_pieces import *
 from board import Board
-from player import Player
+from proj3_chess.piece_properties import Player
 from random import randint, choice
 
 
 def generate_random_board(
-    min_size: int, max_size: int, player1: Player, player2: Player
+    min_size: int,
+    max_size: int,
+    color1: Color,
+    color2: Color,
+    piece_types: Tuple[ChessPiece] = (Rook, Bishop, Knight, Pawn, King, Queen),
 ):
+    width = randint(min_size, max_size)
+    height = randint(min_size, max_size)
+    board = Board(width, height)
 
-    # generate a random board:
-    b = Board(randint(min_size, max_size), randint(min_size, max_size))
-    for x in range(b.width):
-        for y in range(b.height):
-            if randint(0, 1) == True:
-                choice((Rook, Bishop, Knight, Pawn, King, Queen))(
-                    choice((player1, player1)), b, (x, y)
-                )
-    print(b)
-
-
-plr1 = Player(1, lambda x: x.upper() + " ")
-plr2 = Player(-1, lambda y: y.lower() + "*")
-
-generate_random_board(2, 5, plr1, plr2)
+    for x in range(board.width):
+        for y in range(board.height):
+            if randint(0, 1):
+                piece = choice(piece_types)(choice((color1, color2)))
+                piece.add_to_board(board, Spot(x=x, y=y))
+    return board
 
 
-for move in plr1.get_available_moves():
+white = Color(1, lambda x: x.upper() + " ")
+black = Color(-1, lambda y: y.lower() + "*")
+
+generate_random_board(2, 5, white, black)
+
+
+for move in white.get_available_moves():  # TODO: ummmm
     print(move)
