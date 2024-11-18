f6183fcaa3b98f974b6a5f32e0489b98d49e2b1b
irrit-im <meyerovitch.idit@gmail.com>
2024-11-18 20:30:30 +0200

    done a lot (not enough tough)

--- proj3_chess/chess_pieces.py
+++ proj3_chess/chess_pieces.py
@@ -1,63 +1,64 @@
 from board import Board
-from player import Player
+from piece_properties import Color
 from abc import ABC, abstractmethod
-from move import Move
-from typing import Tuple
+from move import Move, Spot, MoveVector
+from typing import Tuple, Iterable
 
 
 class ChessPiece(ABC):
-    """Parent class for all chess pieces."""  # TODO: "But the documentation requires a better description of what the class represents"
+    # TODO: "But the documentation requires a better description of what the class represents" literaly no idea what to write
 
     PIECE_NAME = ""
 
-    def __init__(self, player: Player) -> None:
-        self.player = player
-        self.direction = player.direction
-        # player.pieces.append(self)  # add the piece to the player # TODO: figure out if i actually need this
-        self.name = player.symbolize(self.PIECE_NAME)
+    def __init__(self, color: Color) -> None:
+        self.color = color
+        self.direction = color.direction
+        self.name = color.symbolize(self.PIECE_NAME)
 
-    def add_to_board(
-        self, board: Board, spot: tuple[int, int]
-    ) -> None:  # TODO: figure out spot type
+    def add_to_board(self, board: Board, spot: Spot) -> None:
         self.board = board
-        if self.board[spot[0]][spot[1]]:
+        if self.board[spot.x][spot.y]:
             raise ValueError("spot is already taken")
-        self.board.board_state[spot[0]][spot[1]] = self
+        self.board.board_state[spot.x][spot.y] = self
 
-    def is_legal_move(self, spot: tuple[int, int]) -> bool:
+    def is_legal_move(self, spot: Spot) -> bool:
         if not self.board.is_spot_within_range(spot):
             return False
-        spot_state = self.board.board_state[spot[0]][spot[1]]
-        return spot_state == None or spot_state.player != self.player
+        spot_state = self.board.board_state[spot.x][spot.y]
+        return spot_state == None or spot_state.color != self.color
 
-    def does_capture_piece(self, spot: tuple[int, int]) -> bool:
-        spot_state = self.board.board_state[spot[0]][spot[1]]
-        return spot_state == None or spot_state.player == self.player
+    def does_capture_piece(self, spot: Spot) -> bool:
+        spot_state = self.board.board_state[spot.x][spot.y]
+        return spot_state == None or spot_state.color == self.color
 
-    def get_spot(self) -> tuple[int, int]:
+    def get_spot(self) -> Spot:
         for x in range(self.board.width):
             for y in range(self.board.height):
                 if self.board.board_state[x][y] == self:
-                    return (x, y)
+                    return Spot(x=x, y=y)
 
     @abstractmethod
     def get_available_moves(self) -> list[Move]:
         pass
 
     def get_available_moves_in_directions(
-        self, move_directions: Tuple[Tuple[int, int]]
+        self, move_directions: Iterable[MoveVector]
     ) -> list[Move]:  # TODO: figure out type hinting
         moves = []
-        spot_x, spot_y = self.get_spot()
-        for x_move, y_move in move_directions:
-            next_move = (spot_x + x_move, spot_y + y_move)
+        spot = self.get_spot()
+        for step in move_directions:
+            next_move = spot + step
             while self.is_legal_move(next_move):
                 moves.append(
-                    Move(self.PIECE_NAME, next_move, self.does_capture_piece(next_move))
+                    Move(
+                        destination=next_move,
+                        does_capture_piece=self.does_capture_piece(next_move),
+                        piece_symbol=self.PIECE_NAME,
+                    )
                 )
                 if self.does_capture_piece(next_move):
                     break
-                next_move = (next_move[0] + x_move, next_move[1] + y_move)
+                next_move = next_move + step
         return moves
 
 
@@ -65,7 +66,12 @@ class Rook(ChessPiece):
     PIECE_NAME = "R"
 
     def get_available_moves(self) -> list:
-        move_directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        move_directions = (
+            MoveVector(x=0, y=1),
+            MoveVector(x=0, y=-1),
+            MoveVector(x=1, y=0),
+            MoveVector(x=-1, y=0),
+        )
         return self.get_available_moves_in_directions(move_directions=move_directions)
 
 
@@ -73,11 +79,12 @@ class Bishop(ChessPiece):
     PIECE_NAME = "B"
 
     def get_available_moves(self) -> list[Move]:
+        MV = MoveVector
         move_directions = (
-            (1, 1),
-            (-1, -1),
-            (1, -1),
-            (-1, 1),
+            MV(x=1, y=1),
+            MV(x=-1, y=-1),
+            MV(x=1, y=-1),
+            MV(x=-1, y=1),
         )  # TODO: should make this a class constant?
         return self.get_available_moves_in_directions(move_directions=move_directions)
 
@@ -93,16 +100,17 @@ class King(ChessPiece):
     PIECE_NAME = "K"
 
     def get_available_moves(self) -> list[Move]:
+        spot = self.get_spot()
         moves = []
-        for x in (-1, 0, 1):
-            for y in (-1, 0, 1):
-                next_move = (self.get_spot()[0] + x, self.get_spot()[1] + y)
+        for x_move in (-1, 0, 1):
+            for y_move in (-1, 0, 1):
+                next_move = Spot(spot.x + x_move, spot.y + y_move)
                 if self.is_legal_move(next_move) and next_move != self.get_spot():
                     moves.append(
                         Move(
                             self.PIECE_NAME,
                             next_move,
-                            self.does_capture_piecenext_moves(),
+                            self.does_capture_piece(next_move),
                         )
                     )
         return moves
@@ -111,24 +119,28 @@ class King(ChessPiece):
 class Knight(ChessPiece):
     PIECE_NAME = "N"
     KNIGHT_MOVES = [
-        (1, 2),
-        (2, 1),
-        (-1, 2),
-        (-2, 1),
-        (1, -2),
-        (2, -1),
-        (-1, -2),
-        (-2, -1),
-    ]
+        MoveVector(x=1, y=2),
+        MoveVector(x=1, y=-2),
+        MoveVector(x=2, y=1),
+        MoveVector(x=2, y=-1),
+        MoveVector(x=-1, y=2),
+        MoveVector(x=-1, y=-2),
+        MoveVector(x=-2, y=1),
+        MoveVector(x=-2, y=-1),
+    ]  # TODO: should be class constant?
 
     def get_available_moves(self) -> list[Move]:
-        spot_x, spot_y = self.get_spot()
+        spot = self.get_spot()
         moves = []
-        for x_move, y_move in self.KNIGHT_MOVES:
-            next_move = (spot_x + x_move, spot_y + y_move)
+        for step in self.KNIGHT_MOVES:
+            next_move = spot + step
             if self.is_legal_move(next_move):
                 moves.append(
-                    Move(self.PIECE_NAME, next_move, self.does_capture_piece(next_move))
+                    Move(
+                        destination=next_move,
+                        does_capture_piece=self.does_capture_piece(next_move),
+                        piece_symbol=self.PIECE_NAME,
+                    )
                 )
         return moves
 
@@ -137,15 +149,15 @@ class Pawn(ChessPiece):
     PIECE_NAME = "P"
 
     def get_available_moves(self) -> list[Move]:  # TODO: clean up this mess :)
-        spot_x, spot_y = self.get_spot()
+        spot = self.get_spot()
         moves = []
         # move 1 forward
-        next_move = (spot_x, spot_y + self.player.direction)
+        next_move = Spot(x=spot + self.color.direction)
         if self.is_legal_move(next_move) and not self.does_capture_piece(next_move):
             moves.append(Move(self.PIECE_NAME, next_move, False))
         # capture piece diagonally
         for x_move in (-1, 1):
-            next_move = (spot_x + x_move, spot_y + self.player.direction)
+            next_move = Spot(spot.x + x_move, spot.y + self.color.direction)
             if self.is_legal_move(next_move) and self.does_capture_piece(next_move):
                 moves.append(Move(self.PIECE_NAME, next_move, True))
         # TODO: add case - move 2 forward
