2ae29e9ebc557126d855684b7483231b2a95da51
irrit-im <meyerovitch.idit@gmail.com>
2025-04-22 16:29:26 +0300

    cleanup

--- proj4_s_star/test.py
+++ proj4_s_star/test.py
@@ -11,38 +11,40 @@ MAX_SCREEN_SIZE = 800
 TILE_SIZE_PIXELS = MAX_SCREEN_SIZE // max(BOARD_WIDTH_TILES, BOARD_HIGHT_TILES)
 
 
-start = Vertex.random_vertex(BOARD_WIDTH_TILES, BOARD_HIGHT_TILES)
-end = Vertex.random_vertex(BOARD_WIDTH_TILES, BOARD_HIGHT_TILES)
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode(
+        (TILE_SIZE_PIXELS * BOARD_WIDTH_TILES, TILE_SIZE_PIXELS * BOARD_HIGHT_TILES)
+    )
+    clock = pygame.time.Clock()
+    running = True
 
-obstacles: set[Obstacle] = set()
-for _ in range(randint(0, int(BOARD_HIGHT_TILES * BOARD_WIDTH_TILES * 0.8))):
-    tile = Vertex.random_vertex(BOARD_WIDTH_TILES, BOARD_HIGHT_TILES)
-    if tile != start and tile != end:
-        obstacles.add(tile)
+    start = Vertex.random_vertex(BOARD_WIDTH_TILES, BOARD_HIGHT_TILES)
+    end = Vertex.random_vertex(BOARD_WIDTH_TILES, BOARD_HIGHT_TILES)
+    obstacles: set[Obstacle] = set()
 
-my_graph = Graph(width=BOARD_WIDTH_TILES, height=BOARD_HIGHT_TILES, obstacles=obstacles)
-my_graph_image = GraphSprite(graph=my_graph, total_size_pixels=MAX_SCREEN_SIZE)
-path = a_star(starting_point=start, goal=end, graph=my_graph)
+    for _ in range(randint(0, int(BOARD_HIGHT_TILES * BOARD_WIDTH_TILES * 0.8))):
+        tile = Vertex.random_vertex(BOARD_WIDTH_TILES, BOARD_HIGHT_TILES)
+        if tile != start and tile != end:
+            obstacles.add(tile)
 
-pygame.init()
-screen = pygame.display.set_mode(
-    (TILE_SIZE_PIXELS * BOARD_WIDTH_TILES, TILE_SIZE_PIXELS * BOARD_HIGHT_TILES)
-)
+    my_graph = Graph(
+        width=BOARD_WIDTH_TILES, height=BOARD_HIGHT_TILES, obstacles=obstacles
+    )
+    graph_image = GraphSprite(graph=my_graph, total_size_pixels=MAX_SCREEN_SIZE)
+    path = a_star(starting_point=start, goal=end, graph=my_graph)
 
-clock = pygame.time.Clock()
-running = True
-my_graph_image.draw_grid()
-my_graph_image.draw_path(path=path)
-my_graph_image.draw_obstacles()
-group = pygame.sprite.Group(my_graph_image)
-group.draw(surface=screen)
+    graph_image.draw_grid()
+    graph_image.draw_path(path=path)
+    graph_image.draw_obstacles()
+    group = pygame.sprite.Group(graph_image)
+    group.draw(surface=screen)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
-    clock.tick(10)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
+        clock.tick(10)
 
-
-pygame.quit()
+    pygame.quit()
